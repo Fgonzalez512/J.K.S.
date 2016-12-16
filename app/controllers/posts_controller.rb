37ede1405b91424fe1e9d4b9d3f983468fe3fbce
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-12-16 11:30:29 -0600

    trying something

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,7 +7,7 @@ class PostsController < ApplicationController
   end
 
   def show
-    @post = Post.find(params[:id])
+    @post = Post.find params[:id]
   end
 
   def new
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
 
 
   def edit
-    @post = Post.find(params[:id])
+    @post = Post.find params[:id]
   end
 
   def update
