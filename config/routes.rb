1590fe27b716826c9dfc45c59152c20cbd35baac
Frank Gonzalez <frank.gonzalez628@gmail.com>
2016-12-16 11:17:59 -0600

    trying likes

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations'}
   resources :posts do
     resources :comments
+    member do
+      get 'like'
+    end
   end
   get '/' => 'posts#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
